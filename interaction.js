245d6d3c5ad0e4aaf4bd65a6b9318617f85da59b
KaileyO <kortiz@kippnyccp.org>
2017-06-08 11:29:44 -0400

    Create interaction.js

--- interaction.js
+++ interaction.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
-  console.log("page loaded");
-  $("img").click(function(){
-    console.log('image clicked');
-    $(this).css("width","500px");
-  });
+//   console.log("page loaded");
+//   $("img").click(function(){
+//     console.log('image clicked');
+//     $(this).css("width","500px");
+//   });
+  $("body").append("<p>Testing</p>");
 });
